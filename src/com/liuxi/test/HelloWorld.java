631ce901ec488bea037354e1829e38de15c745d7
liuxi-learnning <liuxi_learnning@aliyun.com>
2020-04-19 09:44:23 +0800

    test conflict left

--- src/com/liuxi/test/HelloWorld.java
+++ src/com/liuxi/test/HelloWorld.java
@@ -1,7 +1,8 @@
 package com.liuxi.test;
 
 public class HelloWorld {
-    public void main(String[] args) {
-        System.out.println("HelloWorld!");
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("left");
     }
 }
